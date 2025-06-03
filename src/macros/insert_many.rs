bfe8f7e1adc9276f7458eba74c66b88d15b9df95
Charles Sibbald <123247+casibbald@users.noreply.github.com>
2025-06-03 13:45:05 +0300

    Restore macro tests and add coverage (#5)

--- src/macros/insert_many.rs
+++ src/macros/insert_many.rs
@@ -9,7 +9,9 @@ macro_rules! lifeguard_insert_many {
     ($pool:expr, $entity:path, $models:expr) => {{
         $pool.execute(|db| {
             Box::pin(async move {
-                let res = $entity::insert_many($models).exec(db).await?;
+                let res = < $entity as sea_orm::EntityTrait >::insert_many($models)
+                    .exec(&db)
+                    .await?;
                 Ok::<_, sea_orm::DbErr>(res.last_insert_id)
             })
         })?
